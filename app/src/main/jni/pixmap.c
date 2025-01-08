e2bbc1355997102b169a983a3e8889c38c8ae98f
Max Schillinger <maxschillinger@web.de>
2025-01-08 10:50:55 +0100

    pixmap: Add comments

--- app/src/main/jni/pixmap.c
+++ app/src/main/jni/pixmap.c
@@ -10,7 +10,7 @@ rgb_to_rgba(const unsigned char *src, int pixel_count)
 		int i_src = i * BYTES_PER_RGB_PIXEL;
 		int i_dst = i * BYTES_PER_RGBA_PIXEL;
 		memcpy(&dst[i_dst], &src[i_src], 3);
-		dst[i_dst + 3] = 0xff;
+		dst[i_dst + 3] = 0xFF;
 	}
 	return dst;
 }
@@ -18,9 +18,10 @@ rgb_to_rgba(const unsigned char *src, int pixel_count)
 unsigned char *
 bitmap_to_rgba(const unsigned char *src, int pixel_count)
 {
+	// initialize with black transparent pixels (0, 0, 0, 0)
 	unsigned char *dst = calloc(pixel_count * BYTES_PER_RGBA_PIXEL, 1);
 	for (int i = 0; i < pixel_count ; i++)
-		if (src[i])
-			dst[i * BYTES_PER_RGBA_PIXEL + 3] = 255;
+		if (src[i]) // black foreground/text
+			dst[i * BYTES_PER_RGBA_PIXEL + 3] = 0xFF;
 	return dst;
 }
