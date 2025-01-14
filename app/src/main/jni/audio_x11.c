e7cdc9106fe6ad3fd573bf841e2009dc4a9dcdc2
Max Schillinger <maxschillinger@web.de>
2025-01-14 22:18:46 +0100

    audio_x11.c: Open PCM device only once

--- app/src/main/jni/audio_x11.c
+++ app/src/main/jni/audio_x11.c
@@ -8,38 +8,17 @@
 #include <vorbis/vorbisfile.h>  // for ov_clear, ov_fopen, ov_info, ov_read
 #include "utils.h"              // for error
 
-static void
-play_ogg(const char *filename)
+static snd_pcm_t *pcm_handle = NULL;
+
+static bool
+init_audio(unsigned int rate, int channels)
 {
-	OggVorbis_File vf;
-	int current_section;
-	char buffer[4096];
-	int bytes_read;
-
-	// Open the .ogg file
-	if (ov_fopen(filename, &vf) < 0) {
-		error("Error opening OGG file: %s", filename);
-		return;
-	}
-
-	// Retrieve audio information
-	vorbis_info *vi = ov_info(&vf, -1);
-	if (!vi) {
-		error("Error retrieving Vorbis information.");
-		ov_clear(&vf);
-		return;
-	}
-
 	// Configure ALSA
-	snd_pcm_t *pcm_handle;
 	snd_pcm_hw_params_t *params;
-	unsigned int rate = vi->rate; // Sampling rate
-	int channels = vi->channels;
 
 	if (snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0) < 0) {
 		error("Error opening PCM device.");
-		ov_clear(&vf);
-		return;
+		return false;
 	}
 
 	snd_pcm_hw_params_malloc(&params);
@@ -50,32 +29,59 @@ play_ogg(const char *filename)
 	snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0);
 
 	if (snd_pcm_hw_params(pcm_handle, params) < 0) {
-		fprintf(stderr, "Error setting PCM parameters.\n");
+		error("Error setting PCM parameters.");
 		snd_pcm_close(pcm_handle);
-		ov_clear(&vf);
 		exit(1);
 	}
 
 	snd_pcm_hw_params_free(params);
+	return true;
+}
+
+static void
+play_ogg(const char *filename)
+{
+	OggVorbis_File vf = {0};
+	int current_section;
+	char buffer[4096];
+	int bytes_read;
+
+	// Open the .ogg file
+	if (vf.datasource == NULL) {
+		if (ov_fopen(filename, &vf) < 0) {
+			error("Error opening OGG file: %s", filename);
+			return;
+		}
+	}
+
+	// Retrieve audio information
+	vorbis_info *vi = ov_info(&vf, -1);
+	if (!vi) {
+		error("Error retrieving Vorbis information.");
+		ov_clear(&vf);
+		return;
+	}
+
+	if (pcm_handle == NULL && init_audio(vi->rate, vi->channels) == false) {
+		ov_clear(&vf);
+		return;
+	}
 
 	// Decode OGG data and play
 	while ((bytes_read = ov_read(&vf, buffer, sizeof(buffer), 0, 2, 1, &current_section)) > 0) {
-		int frames = bytes_read / (2 * channels); // 2 bytes per sample
+		int frames = bytes_read / (2 * vi->channels); // 2 bytes per sample
 		if (snd_pcm_writei(pcm_handle, buffer, frames) == -EPIPE) {
 			snd_pcm_prepare(pcm_handle);
 		}
 	}
 
-	// Clean up
-	snd_pcm_drain(pcm_handle);
-	snd_pcm_close(pcm_handle);
 	ov_clear(&vf);
 }
 
 void
 play_audio(const char *filename)
 {
-	if (!strstr(filename, ".ogg")) {
+	if (!endswith(".ogg", filename)) {
 		error("Only ogg files are supported!");
 		return;
 	}
@@ -85,6 +91,14 @@ play_audio(const char *filename)
 	play_ogg(adapted_path);
 }
 
+void
+destroy_audio_player()
+{
+	if (pcm_handle) {
+		snd_pcm_drain(pcm_handle);
+		snd_pcm_close(pcm_handle);
+	}
+}
+
 void create_audio_engine() {}
-void destroy_audio_player() {}
 void destroy_audio_engine() {}
